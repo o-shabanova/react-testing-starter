afb0ec515cd5216ebbddd8b8ce6a70b85b6a5097
Olga Shabanova <shabanova994@gmail.com>
2024-11-13 14:33:56 +0200

    Complete writing OrderStatusSelector tests and add some configs to eslint

--- tests/components/OrderStatusSelector.test.tsx
+++ tests/components/OrderStatusSelector.test.tsx
@@ -6,9 +6,10 @@ import {expect} from "vitest";
 
 describe('OrderStatusSelector', ()=> {
     const renderComponent = () => {
+        const onChange = vi.fn();
         render(
             <Theme>
-                <OrderStatusSelector onChange={vi.fn()}/>
+                <OrderStatusSelector onChange={onChange}/>
             </Theme>
         );
 
@@ -20,8 +21,10 @@ describe('OrderStatusSelector', ()=> {
             // no options rendered in the DOM.
             // So we use function (lazy evaluation) there to postpone
             // the execution of this piece of code
-            getOptions: () => screen.findAllByRole('option')
-
+            getOptions: () => screen.findAllByRole('option'),
+            getOption: (label: RegExp) => screen.findByRole('option', {name: label}),
+            user: userEvent.setup(),
+            onChange
         }
     }
     it('should render New as default value', ()=> {
@@ -32,14 +35,44 @@ describe('OrderStatusSelector', ()=> {
     });
 
     it('should render correct statuses', async ()=> {
-        const {trigger, getOptions} = renderComponent();
+        const {trigger, getOptions, user} = renderComponent();
 
-        const user = userEvent.setup();
-        await user.click(trigger);
+         await user.click(trigger);
 
         const options = await getOptions();
         expect(options).toHaveLength(3);
         const labels = options.map(option => option.textContent);
         expect(labels).toEqual(['New', 'Processed', 'Fulfilled']);
     });
+
+    //user interaction tests
+    it.each([
+        {label: /processed/i, value: 'processed'},
+        {label: /fulfilled/i, value: 'fulfilled'},
+    ])('should call onChange with $value when the $label option is selected', async({ label, value }) => {
+        const {trigger, user, onChange, getOption} = renderComponent();
+        await user.click(trigger);
+
+        const option = await getOption(label);
+        await user.click(option);
+
+        expect(onChange).toHaveBeenCalledWith(value);
+    });
+
+    //but we can`t do the same thing with new option, because 'new' is the default option and before test that option,
+    // we have to choose another option to change to 'new' option again
+
+    it(`should call onChange with 'new' when the New option is selected`, async () => {
+        const {trigger, user, onChange, getOption } = renderComponent();
+        await user.click(trigger);
+
+            const processedOption = await getOption(/processed/i);
+            await user.click(processedOption);
+
+            await user.click(trigger);
+            const newOption = await getOption(/new/i);
+            await user.click(newOption);
+
+            expect(onChange).toHaveBeenCalledWith('new');
+    })
 });
